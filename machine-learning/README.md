f9002e89499ce746cc09eb965a266599b0a56e6c
Michael Taylor <michael.a.taylor@uconn.edu>
2024-03-17 09:55:35 -0700

    add aml online endpoints

---
+++ machine-learning/README.md
@@ -0,0 +1,1 @@
+[Deploy and score a machine learning model by using an online endpoint](https://learn.microsoft.com/en-us/azure/machine-learning/how-to-deploy-online-endpoints?view=azureml-api-2&tabs=azure-cli)
--- machine-learning/onlinescoring/score.py
+++ machine-learning/onlinescoring/score.py
@@ -1,41 +1,26 @@
 import os
-import logging
 import json
-import numpy
+import numpy as np
 import joblib
 
 
 def init():
-    """
-    This function is called when the container is initialized/started, typically after create/update of the deployment.
-    You can write the logic here to perform init operations like caching the model in memory
-    """
-    global model
-    # AZUREML_MODEL_DIR is an environment variable created during deployment.
-    # It is the path to the model folder (./azureml-models/$MODEL_NAME/$VERSION)
-    # Please provide your model's folder name if there is one
-    model_path = os.path.join(
-        os.getenv("AZUREML_MODEL_DIR"), "model/sklearn_regression_model.pkl"
-    )
-    # deserialize the model file back into a sklearn model
-    model = joblib.load(model_path)
-    logging.info("Init complete")
+    global mens_models
+    global womens_models
+
+    mens_model_dir, womens_model_dir = os.path.join(os.getenv("AZUREML_MODEL_DIR"), "model/mens/"), os.path.join(os.getenv("AZUREML_MODEL_DIR"), "model/womens/")
+    mens_filenames, womens_filenames = [filename for filename in os.listdir(mens_model_dir)], [filename for filename in os.listdir(womens_model_dir)]
+    mens_models, womens_models = {filename.split('.pkl')[0]: joblib.load(f'{mens_model_dir}{filename}') for filename in mens_filenames}, {filename.split('.pkl')[0]: joblib.load(f'{womens_model_dir}{filename}') for filename in womens_filenames}
 
 
 def run(raw_data):
-    """
-    This function is called for every invocation of the endpoint to perform the actual scoring/prediction.
-    In the example we extract the data from the json input and call the scikit-learn model's predict()
-    method and return the result back
-    """
-    logging.info("model 2: request received")
-    result = [0.5, 0.5]
-    logging.info("Request processed")
-    # return hardcoded result so that it is easy to validate safe rollout scenario: https://docs.microsoft.com/en-us/azure/machine-learning/how-to-safely-rollout-managed-endpoints
-    return result
-
-    # actual scoring logic for reference:
-    # data = json.loads(raw_data)["data"]
-    # data = numpy.array(data)
-    # result = model.predict(data)
-    # return result.tolist()
+    results = []
+    payload = json.loads(raw_data)
+    for matchup in payload:
+        input = np.array([matchup['team2'] + matchup['team1'] + [int(matchup['isNeutral'])]])
+        models = mens_models if not matchup['isWomens'] else womens_models
+        model = models[matchup['model']]
+        predict, predict_proba = model.predict(input), model.predict_proba(input)
+        matchup['predict'], matchup['predictProba'] = predict.tolist(), predict_proba.tolist()[0]
+        results.append(matchup)
+    return results
